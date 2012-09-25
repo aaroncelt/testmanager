c3fdb782b53d23c4f2ca90f4d7f051bdde1011df
tarcsanyi.laszlo@gmail.com <tarcsanyi.laszlo@gmail.com@7156e0a1-2eb8-1cb3-4f55-70f779ba6c89>
2012-09-25 18:47:38 +0000

    Result index page refactor done

--- trunk/src/main/webapp/js/results/index.js
+++ trunk/src/main/webapp/js/results/index.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
-	$('div#filterForm').toggle();
-	$('div#filterFormText').click(
-			function() {
-				$('div#filterForm').toggle(1000);
-			});
+	$('#filterForm').toggle();
+	$('#setList').toggle();
+
+	$('#filterFormText').click(function() {
+		$('#filterForm').toggle(1000);
+	});
+
+	$('#setListText').click(function() {
+		$('#setList').toggle(1000);
+	});
+
+	$('button.filterLink').each(function(i, element) {
+		$(element).click(function() {
+			 var tmp = $(this).attr('filterType')
+											+ "?filterKey="
+											+ $('#headFilterSelect').val()
+											+ "&filterValue="
+											+ $('#headFilterArea').val();
+									window.location.href = tmp;
+
+		});
+	});
+
 });
 
 function deleteSelected(setId) {
-    var r = confirm("Are you sure?");
-    if (r == true) {
-        window.location.href = "delete?setId=" + setId;
-    }
-}
-
-function filterSelected(filterLink) {
-    var temp = $('#' + filterLink).attr('href') + "?filterKey=" + $('#headFilterSelect').val() + "&filterValue=" + $('#headFilterArea').val();
-    $('#' + filterLink).attr('href', temp);
+	var r = confirm("Are you sure?");
+	if (r == true) {
+		window.location.href = "delete?setId=" + setId;
+	}
 }
