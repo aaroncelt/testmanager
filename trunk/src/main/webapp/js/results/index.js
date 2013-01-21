44bbe1b0b972686fdbbe6d9e093bc213f9695282
tarcsanyi.laszlo@gmail.com <tarcsanyi.laszlo@gmail.com@7156e0a1-2eb8-1cb3-4f55-70f779ba6c89>
2013-01-21 20:29:46 +0000

    Result/index page UI changes. Remove table layout.

--- trunk/src/main/webapp/js/results/index.js
+++ trunk/src/main/webapp/js/results/index.js
@@ -1,32 +1,56 @@
-$(document).ready(function() {
-	$('#filterForm').toggle();
-	$('#setList').toggle();
+$(document).ready(
+        function() {
+            $('#filterForm').toggle();
+            $('#setList').toggle();
 
-	$('#filterFormText').click(function() {
-		$('#filterForm').toggle();
-	});
+            $('#filterFormText').click(function() {
+                $('#filterForm').toggle();
+            });
 
-	$('#setListText').click(function() {
-		$('#setList').toggle();
-	});
+            $('#setListText').click(function() {
+                $('#setList').toggle();
+            });
 
-	$('button.filterLink').each(function(i, element) {
-		$(element).click(function() {
-			 var tmp = $(this).attr('filterType')
-											+ "?filterKey="
-											+ $('#headFilterSelect').val()
-											+ "&filterValue="
-											+ $('#headFilterArea').val();
-									window.location.href = tmp;
+            $('button.filterLink').each(
+                    function(i, element) {
+                        $(element).click(
+                                function() {
+                                    var tmp = $(this).attr('filterType')
+                                    + "?filterKey="
+                                    + $('#headFilterSelect').val()
+                                    + "&filterValue="
+                                    + $('#headFilterArea').val();
+                                    window.location.href = tmp;
+                                });
+                    });
 
-		});
-	});
-
-});
+            $('#selectedSet').change(function() {
+                var toShow = [];
+                $("#selectedSet option:selected").each(function() {
+                    toShow.push($(this).val());
+                });
+                console.log(toShow);
+                $.cookie("selected-sets",toShow.join("|"));
+                if ("all" === $("#selectedSet option:selected").val()) {
+                    $('.set-results-box').each(function() {
+                        $(this).show();
+                    });
+                } else {
+                    $('.set-results-box').each(function() {
+                        console.log($(this).attr('id'));
+                        if ($.inArray($(this).attr('id'), toShow) === -1) {
+                            $(this).hide();
+                        } else {
+                            $(this).show();
+                        }
+                    });
+                }
+            });
+        });
 
 function deleteSelected(setId) {
-	var r = confirm("Are you sure?");
-	if (r == true) {
-		window.location.href = "delete?setId=" + setId;
-	}
+    var r = confirm("Are you sure?");
+    if (r == true) {
+        window.location.href = "delete?setId=" + setId;
+    }
 }
