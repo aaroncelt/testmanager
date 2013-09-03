4dfc61399891596e81e26c209cbf837bb4a0e463
tarcsanyi.laszlo@gmail.com <tarcsanyi.laszlo@gmail.com@7156e0a1-2eb8-1cb3-4f55-70f779ba6c89>
2013-09-03 13:59:04 +0000

    Labels reporting for test implemented

--- branches/testmanager-clientdriver-java/src/testmanager/reporting/client/ReportManager.java
+++ branches/testmanager-clientdriver-java/src/testmanager/reporting/client/ReportManager.java
@@ -29,225 +29,249 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
- * The Class ReportManager manages the reporting of test and set starts or stops towards TestManager.
- *
+ * The Class ReportManager manages the reporting of test and set starts or stops
+ * towards TestManager.
+ * 
  * @author Istvan_Pamer
  */
 public class ReportManager {
 
-    private static final String REQUEST_METHOD = "POST";
-    private static final String RESPONSE_OK = "OK";
-    private static final String DEFAULT_ENCODING = "UTF-8";
-    private static final String URI_AVAILABILITY_MAIN = "/availability/main";
-    private static final String URI_REPORTING_START = "/reporting/start";
-    private static final String URI_REPORTING_STOP = "/reporting/stop";
-    private static final String PARAM_TESTNAME = "testName=";
-    private static final String PARAM_PARAMNAME = "paramName=";
-    private static final String PARAM_SETNAME = "setName=";
-    private static final String PARAM_SETSTARTDATE = "setStartDate=";
-    private static final String PARAM_RESULTSTATE = "result=";
-    private static final String PARAM_TESTPARAMS = "testParams=";
-    private static final String PARAM_MSG = "msg=";
-    private static final String PARAM_ENV = "env=";
-    private static final String PARAM_CHKPOINTS = "checkPoints=";
-    private static final String REPORT_DATE_FORMAT_STRING = "yyyy.MM.dd;HH:mm:ss.SSS";
-    private static final int TIMEOUT_MAIN_SERVICE = 5000;
-    private static final int TIMEOUT_REPORTING = 60000;
+	private static final String REQUEST_METHOD = "POST";
+	private static final String RESPONSE_OK = "OK";
+	private static final String DEFAULT_ENCODING = "UTF-8";
+	private static final String URI_AVAILABILITY_MAIN = "/availability/main";
+	private static final String URI_REPORTING_START = "/reporting/start";
+	private static final String URI_REPORTING_STOP = "/reporting/stop";
+	private static final String PARAM_TESTNAME = "testName";
+	private static final String PARAM_PARAMNAME = "paramName";
+	private static final String PARAM_SETNAME = "setName";
+	private static final String PARAM_SETSTARTDATE = "setStartDate";
+	private static final String PARAM_RESULTSTATE = "result";
+	private static final String PARAM_TESTPARAMS = "testParams";
+	private static final String PARAM_MSG = "msg";
+	private static final String PARAM_ENV = "env";
+	private static final String PARAM_CHKPOINTS = "checkPoints";
+	private static final String PARAM_LABELS = "labels";
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MM.dd;HH:mm:ss.SSS");
+	private static final int TIMEOUT_MAIN_SERVICE = 5000;
+	private static final int TIMEOUT_REPORTING = 60000;
 
-    private String uriRoot = "/TestManager/app";
-    private String host;
-    private String port;
-    private int timeoutMainService = TIMEOUT_MAIN_SERVICE;
-    private int timeoutReporting = TIMEOUT_REPORTING;
+	private String uriRoot = "/TestManager/app";
+	private String host;
+	private String port;
+	private int timeoutMainService = TIMEOUT_MAIN_SERVICE;
+	private int timeoutReporting = TIMEOUT_REPORTING;
 
-    public enum ResultState {
-        PASSED, FAILED, NOT_AVAILABLE, ABORTED;
-    }
+	public enum ResultState {
+		PASSED, FAILED, NOT_AVAILABLE, ABORTED;
+	}
 
-    /**
-     * Instantiates a new report manager.
-     *
-     * @param host the host
-     * @param port the port
-     */
-    public ReportManager(String host, String port) {
-        this.host = host;
-        this.port = port;
-    }
+	/**
+	 * Instantiates a new report manager.
+	 * 
+	 * @param host the host
+	 * @param port the port
+	 */
+	public ReportManager(String host, String port) {
+		this.host = host;
+		this.port = port;
+	}
 
-    /**
-     * Instantiates a new report manager.
-     *
-     * @param host the host
-     * @param port the port
-     * @param uriRoot the uri of the application root
-     */
-    public ReportManager(String host, String port, String uriRoot) {
-        this.host = host;
-        this.port = port;
-        this.uriRoot = uriRoot;
-    }
+	/**
+	 * Instantiates a new report manager.
+	 * 
+	 * @param host the host
+	 * @param port the port
+	 * @param uriRoot the uri of the application root
+	 */
+	public ReportManager(String host, String port, String uriRoot) {
+		this.host = host;
+		this.port = port;
+		this.uriRoot = uriRoot;
+	}
 
-    /**
-     * Sets the timeout of checking if the main service is online. Default is 3000 ms.
-     *
-     * @param timeoutMainService the new timeout main service
-     */
-    public void setTimeoutMainService(int timeoutMainService) {
-        if (timeoutMainService > 0) {
-            this.timeoutMainService = timeoutMainService;
-        }
-    }
+	/**
+	 * Sets the timeout of checking if the main service is online. Default is
+	 * 3000 ms.
+	 * 
+	 * @param timeoutMainService the new timeout main service
+	 */
+	public void setTimeoutMainService(int timeoutMainService) {
+		if (timeoutMainService > 0) {
+			this.timeoutMainService = timeoutMainService;
+		}
+	}
 
-    /**
-     * Sets the timeout for reporting. Default is 30000 ms.
-     *
-     * @param timeoutReporting the new timeout reporting
-     */
-    public void setTimeoutReporting(int timeoutReporting) {
-        if (timeoutReporting > 0) {
-            this.timeoutReporting = timeoutReporting;
-        }
-    }
+	/**
+	 * Sets the timeout for reporting. Default is 30000 ms.
+	 * 
+	 * @param timeoutReporting the new timeout reporting
+	 */
+	public void setTimeoutReporting(int timeoutReporting) {
+		if (timeoutReporting > 0) {
+			this.timeoutReporting = timeoutReporting;
+		}
+	}
 
-    /**
-     * Checks if the main service is reachable.
-     *
-     * @return true, if the main service is reachable
-     */
-    public boolean isMainServiceReachable() {
-        return (RESPONSE_OK.equals(readUri(URI_AVAILABILITY_MAIN, timeoutMainService))) ? true : false;
-    }
+	/**
+	 * Checks if the main service is reachable.
+	 * 
+	 * @return true, if the main service is reachable
+	 */
+	public boolean isMainServiceReachable() {
+		return (RESPONSE_OK.equals(readUri(URI_AVAILABILITY_MAIN, timeoutMainService))) ? true : false;
+	}
 
-    /**
-     * Report test start.
-     *
-     * @param testName the test name
-     * @param paramName the param name
-     * @param setName the set name
-     * @param setStartDate the set start date
-     * @param optionals the optionals
-     * @return true, if successful
-     */
-    public boolean reportTestStart(String testName, String paramName, String setName, Date setStartDate, ReportParams optionals) {
-        // EXAMPLE:
-        // http://localhost:8080/TestManager/app/reporting/start?testName=test1&setName=smoke&setStartDate=2011.08.15;15:50:49.123
-        boolean result = false;
-        String urlParams;
-        try {
-            // if some of them fail, skip request
-            urlParams = PARAM_TESTNAME + URLEncoder.encode(testName, DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_PARAMNAME + URLEncoder.encode(paramName, DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_SETNAME + URLEncoder.encode(setName, DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_SETSTARTDATE + URLEncoder.encode(new SimpleDateFormat(REPORT_DATE_FORMAT_STRING).format(setStartDate), DEFAULT_ENCODING);
-            if (optionals != null) {
-                if (optionals.getTestParams() != null) {
-                    urlParams += "&" + PARAM_TESTPARAMS + URLEncoder.encode(optionals.getTestParams(), DEFAULT_ENCODING);
-                }
-                if (optionals.getEnvironment() != null) {
-                    urlParams += "&" + PARAM_ENV + URLEncoder.encode(optionals.getEnvironment(), DEFAULT_ENCODING);
-                }
-            }
-            result = (RESPONSE_OK.equals(readUri(URI_REPORTING_START, urlParams, timeoutReporting))) ? true : false;
-        } catch (UnsupportedEncodingException e) {
-            //e.printStackTrace();
-        }
-        return result;
-    }
+	/**
+	 * Report test start.
+	 * 
+	 * @param testName the test name
+	 * @param paramName the param name
+	 * @param setName the set name
+	 * @param setStartDate the set start date
+	 * @param optionals the optionals
+	 * @return true, if successful
+	 */
+	public boolean reportTestStart(String testName, String paramName, String setName, Date setStartDate, ReportParams optionals) {
+		boolean result = false;
+		Map<String, String> urlParams = new HashMap<String, String>();
+		try {
+			urlParams.put(PARAM_TESTNAME, testName);
+			urlParams.put(PARAM_PARAMNAME, paramName);
+			urlParams.put(PARAM_SETNAME, setName);
+			urlParams.put(PARAM_SETSTARTDATE, DATE_FORMAT.format(setStartDate));
+			if (optionals != null) {
+				if (optionals.getTestParams() != null) {
+					urlParams.put(PARAM_TESTPARAMS, optionals.getTestParams());
+				}
+				if (optionals.getEnvironment() != null) {
+					urlParams.put(PARAM_ENV, optionals.getEnvironment());
+				}
+				if (optionals.hasLabels()) {
+					urlParams.put(PARAM_LABELS, optionals.getLabels());
+				}
+			}
+			result = RESPONSE_OK.equals(readUri(URI_REPORTING_START, buildUrlParamString(urlParams), timeoutReporting));
+		} catch (UnsupportedEncodingException e) {
+			// if some of them failed, skip request
+		}
+		return result;
+	}
 
-    /**
-     * Report test stop.
-     *
-     * @param testName the test name
-     * @param paramName the param name
-     * @param setName the set name
-     * @param setStartDate the set start date
-     * @param resultState the result state
-     * @param optionals the optionals
-     * @return true, if successful
-     */
-    public boolean reportTestStop(String testName, String paramName, String setName, Date setStartDate, ResultState resultState, ReportParams optionals) {
-        // EXAMPLE:
-        // http://localhost:8080/TestManager/app/reporting/stop?testName=test1&setName=smoke&setStartDate=2011.08.15;15:50:49.123&result=PASSED
-        boolean result = false;
-        String urlParams;
-        try {
-            // if some of them fail, skip request
-            urlParams = PARAM_TESTNAME + URLEncoder.encode(testName, DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_PARAMNAME + URLEncoder.encode(paramName, DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_SETNAME + URLEncoder.encode(setName, DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_SETSTARTDATE + URLEncoder.encode(new SimpleDateFormat(REPORT_DATE_FORMAT_STRING).format(setStartDate), DEFAULT_ENCODING);
-            urlParams += "&" + PARAM_RESULTSTATE + URLEncoder.encode(resultState.toString(), DEFAULT_ENCODING);
-            if (optionals != null) {
-                if (optionals.getTestParams() != null) {
-                    urlParams += "&" + PARAM_TESTPARAMS + URLEncoder.encode(optionals.getTestParams(), DEFAULT_ENCODING);
-                }
-                if (optionals.getErrorMessage() != null) {
-                    urlParams += "&" + PARAM_MSG + URLEncoder.encode(optionals.getErrorMessage(), DEFAULT_ENCODING);
-                }
-                if (optionals.getEnvironment() != null) {
-                    urlParams += "&" + PARAM_ENV + URLEncoder.encode(optionals.getEnvironment(), DEFAULT_ENCODING);
-                }
-                if (optionals.hasCheckPoints()) {
-                    urlParams += "&" + PARAM_CHKPOINTS + URLEncoder.encode(optionals.getCheckPoints(), DEFAULT_ENCODING);
-                }
-            }
-            result = (RESPONSE_OK.equals(readUri(URI_REPORTING_STOP, urlParams, timeoutReporting))) ? true : false;
-        } catch (UnsupportedEncodingException e) {
-            //e.printStackTrace();
-        }
-        return result;
-    }
+	/**
+	 * Report test stop.
+	 * 
+	 * @param testName the test name
+	 * @param paramName the param name
+	 * @param setName the set name
+	 * @param setStartDate the set start date
+	 * @param resultState the result state
+	 * @param optionals the optionals
+	 * @return true, if successful
+	 */
+	public boolean reportTestStop(String testName, String paramName, String setName, Date setStartDate, ResultState resultState,
+			ReportParams optionals) {
+		boolean result = false;
+		Map<String, String> urlParams = new HashMap<String, String>();
+		try {
+			urlParams.put(PARAM_TESTNAME, testName);
+			urlParams.put(PARAM_PARAMNAME, paramName);
+			urlParams.put(PARAM_SETNAME, setName);
+			urlParams.put(PARAM_SETSTARTDATE, DATE_FORMAT.format(setStartDate));
+			urlParams.put(PARAM_RESULTSTATE, resultState.toString());
+			if (optionals != null) {
+				if (optionals.getTestParams() != null) {
+					urlParams.put(PARAM_TESTPARAMS, optionals.getTestParams());
+				}
+				if (optionals.getErrorMessage() != null) {
+					urlParams.put(PARAM_MSG, optionals.getErrorMessage());
+				}
+				if (optionals.getEnvironment() != null) {
+					urlParams.put(PARAM_ENV, optionals.getEnvironment());
+				}
+				if (optionals.hasCheckPoints()) {
+					urlParams.put(PARAM_CHKPOINTS, optionals.getCheckPoints());
+				}
+				if (optionals.hasLabels()) {
+					urlParams.put(PARAM_LABELS, optionals.getLabels());
+				}
+			}
+			result = RESPONSE_OK.equals(readUri(URI_REPORTING_STOP, buildUrlParamString(urlParams), timeoutReporting));
+		} catch (UnsupportedEncodingException e) {
+			// if some of them fail, skip request
+		}
+		return result;
+	}
 
-    private String readUri(String uri, int timeout) {
-        return readUri(uri, null, timeout);
-    }
+	private String readUri(String uri, int timeout) {
+		return readUri(uri, null, timeout);
+	}
 
-    private String readUri(String uri, String urlParams, int timeout) {
-        String result = null;
-        HttpURLConnection conn = null;
+	private String readUri(String uri, String urlParams, int timeout) {
+		String result = null;
+		HttpURLConnection conn = null;
 
-        try {
-            URL url = new URL("http://" + host + ":" + port + uriRoot + uri);
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setConnectTimeout(timeout);
-            conn.setRequestMethod(REQUEST_METHOD);
-            conn.setUseCaches (false);
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
+		try {
+			URL url = new URL("http://" + host + ":" + port + uriRoot + uri);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(timeout);
+			conn.setRequestMethod(REQUEST_METHOD);
+			conn.setUseCaches(false);
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
 
-            //Send request
-            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-            if (urlParams != null) {
-                wr.writeBytes(urlParams);
-            }
-            wr.flush();
-            wr.close();
+			// Send request
+			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+			if (urlParams != null) {
+				wr.writeBytes(urlParams);
+			}
+			wr.flush();
+			wr.close();
 
-            //Get Response
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), DEFAULT_ENCODING));
-            String line;
-            StringBuilder response = new StringBuilder();
-            while((line = rd.readLine()) != null) {
-              response.append(line);
-            }
-            rd.close();
-            result = response.toString();
-        } catch (MalformedURLException e) {
-            //e.printStackTrace();
-        } catch (ProtocolException e) {
-            //e.printStackTrace();
-        } catch (IOException e) {
-            //e.printStackTrace();
-        } finally {
-            if(conn != null) {
-                conn.disconnect();
-            }
-        }
+			// Get Response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), DEFAULT_ENCODING));
+			String line;
+			StringBuilder response = new StringBuilder();
+			while ((line = rd.readLine()) != null) {
+				response.append(line);
+			}
+			rd.close();
+			result = response.toString();
+		} catch (MalformedURLException e) {
+			// e.printStackTrace();
+		} catch (ProtocolException e) {
+			// e.printStackTrace();
+		} catch (IOException e) {
+			// e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
+	private String buildUrlParamString(Map<String, String> params) throws UnsupportedEncodingException {
+		String retval = "";
+		Set<String> paramValuePairs = new HashSet<String>();
+		if (params != null) {
+			for (Entry<String, String> entry : params.entrySet()) {
+				paramValuePairs.add(entry.getKey() + "=" + URLEncoder.encode(entry.getValue(), DEFAULT_ENCODING));
+			}
+		}
+		if (!paramValuePairs.isEmpty()) {
+			retval = StringUtils.join(paramValuePairs, "&");
+		}
+		return retval;
+	}
 }
--- branches/testmanager-clientdriver-java/src/testmanager/reporting/client/ReportParams.java
+++ branches/testmanager-clientdriver-java/src/testmanager/reporting/client/ReportParams.java
@@ -20,84 +20,111 @@ package testmanager.reporting.client;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 /**
  * The Class ReportParams.
- *
+ * 
  * @author Istvan_Pamer
  */
 public class ReportParams {
 
-    private static final String MESSAGE = "message";
-    private static final String MAIN_TYPE = "mainType";
-    private static final String SUB_TYPE = "subType";
-    private static final String STATE = "state";
+	private static final String MESSAGE = "message";
+	private static final String MAIN_TYPE = "mainType";
+	private static final String SUB_TYPE = "subType";
+	private static final String STATE = "state";
 
-    private String errorMessage;							// test run error message if test not passes
-    private JSONObject environment = new JSONObject();		// environment variables
-    private JSONObject testParams = new JSONObject();		// parameters for the test run
-    private List<String> checkPoints = new ArrayList<String>();		// check points / verification points
+	private String errorMessage;
+	private JSONObject environment = new JSONObject();
+	private JSONObject testParams = new JSONObject();
+	private List<String> checkPoints = new ArrayList<String>();
+	private List<String> labels = new ArrayList<String>();
 
-    /**
-     * Clears the parameter object in order to reuse it.
-     *
-     * @return self
-     */
-    public ReportParams clearParams() {
-        errorMessage = null;
-        environment = new JSONObject();
-        testParams = new JSONObject();
-        checkPoints = new ArrayList<String>();
-        return this;
-    }
+	/**
+	 * Clears the parameter object in order to reuse it.
+	 * 
+	 * @return self
+	 */
+	public ReportParams clearParams() {
+		errorMessage = null;
+		environment = new JSONObject();
+		testParams = new JSONObject();
+		checkPoints = new ArrayList<String>();
+		labels = new ArrayList<String>();
+		return this;
+	}
 
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-    public ReportParams setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-        return this;
-    }
+	public String getErrorMessage() {
+		return errorMessage;
+	}
 
-    public String getTestParams() {
-        return testParams.toJSONString();
-    }
-    public ReportParams setTestParam(String key, String value) {
-        testParams.put(key, value);
-        return this;
-    }
+	public ReportParams setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+		return this;
+	}
 
-    public String getEnvironment() {
-        return environment.toJSONString();
-    }
-    public ReportParams setEnvironment(String key, String value) {
-        environment.put(key, value);
-        return this;
-    }
+	public String getTestParams() {
+		return testParams.toJSONString();
+	}
 
-    public boolean hasCheckPoints() {
-        return !checkPoints.isEmpty();
-    }
-    public String getCheckPoints() {
-        return "[" + StringUtils.join(checkPoints, ", ") + "]";
-    }
-    public ReportParams setCheckPoint(String message, String mainType, String subType, CheckPointState state) {
-        JSONObject temp = new JSONObject();
-        temp.put(MESSAGE, message);
-        temp.put(MAIN_TYPE, mainType);
-        temp.put(SUB_TYPE, subType);
-        temp.put(STATE, state.toString());
-        checkPoints.add(temp.toJSONString());
-        return this;
-    }
+	@SuppressWarnings("unchecked")
+	public ReportParams setTestParam(String key, String value) {
+		testParams.put(key, value);
+		return this;
+	}
 
-    /**
-     * The Enum CheckPointState.
-     */
-    public enum CheckPointState {
-        PASSED, FAILED
-    }
+	public String getEnvironment() {
+		return environment.toJSONString();
+	}
+
+	@SuppressWarnings("unchecked")
+	public ReportParams setEnvironment(String key, String value) {
+		environment.put(key, value);
+		return this;
+	}
+
+	public boolean hasCheckPoints() {
+		return !checkPoints.isEmpty();
+	}
+
+	public String getCheckPoints() {
+		return "[" + StringUtils.join(checkPoints, ", ") + "]";
+	}
+
+	@SuppressWarnings("unchecked")
+	public ReportParams setCheckPoint(String message, String mainType, String subType, CheckPointState state) {
+		JSONObject temp = new JSONObject();
+		temp.put(MESSAGE, message);
+		temp.put(MAIN_TYPE, mainType);
+		temp.put(SUB_TYPE, subType);
+		temp.put(STATE, state.toString());
+		checkPoints.add(temp.toJSONString());
+		return this;
+	}
+
+	public boolean hasLabels() {
+		return !labels.isEmpty();
+	}
+
+	public String getLabels() {
+		return JSONArray.toJSONString(labels);
+	}
+
+	public void addLabel(String label) {
+		this.labels.add(label);
+	}
+
+	public void addLabels(List<String> labels) {
+		this.labels.addAll(labels);
+	}
+
+	/**
+	 * The Enum CheckPointState.
+	 */
+	public enum CheckPointState {
+		PASSED, FAILED
+	}
 
 }
