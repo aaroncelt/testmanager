3039a35eac35572f58bb8643505e1e4526b0c149
tarcsanyi.laszlo <tarcsanyi.laszlo@gmail.com>
2013-10-04 13:54:13 +0000

    Filter extended to accept multiple keywords separated by comma

---
+++ src/main/webapp/js/move_title.js
@@ -0,0 +1,23 @@
+var speed = 150;
+var endChar = " ";
+var pos = 0;
+ 
+function moveTitle(msg)
+{
+  console.log(msg);
+  var ml = msg.length;
+       
+  title = msg.substr(pos,ml) + endChar + msg.substr(0,pos);
+  document.title = title;
+   
+  pos++;
+  if (pos > ml) {
+	  pos=0;
+  }
+  window.setTimeout(function() {
+	  moveTitle(msg);
+  },speed);
+}
+$(document).ready(function() {
+  moveTitle(document.title);
+});
